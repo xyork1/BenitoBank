6b3d03c25e9accc5c7c417f83c93c44809b648fe
xYorkx <york31625@gmail.com>
2025-04-17 01:21:20 -0500

    Inicio de aplicación

---
+++ src/main/java/com/remington/benitobank/BenitobankApplication.java
@@ -0,0 +1,13 @@
+package com.remington.benitobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenitobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenitobankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remington/benitobank/BenitobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remington.benitobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenitobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
